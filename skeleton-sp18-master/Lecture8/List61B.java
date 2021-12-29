71e84a9cdb3a878ed29f8f1bb181955e2c90391c
kkgogogo <kang.zibin@gmail.com>
2021-12-29 14:30:16 -0500

    add Lecture8 document
    add interface List61B

---
+++ skeleton-sp18-master/Lecture8/List61B.java
@@ -0,0 +1,10 @@
+public interface List61B<Item> {
+    public void addLast(Item x);
+    public Item getLast();
+    public int size();
+    public Item removeLast();
+    public void insert(Item x, int position);
+    public void addFirst(Item x);
+    public void getFirst();
+
+}
