678c2541efb0c1a92f19a8a4e6fe8bf4add491b2
kkgogogo <kang.zibin@gmail.com>
2021-12-29 15:11:35 -0500

    add default method print()

--- skeleton-sp18-master/Lecture8/List61B.java
+++ skeleton-sp18-master/Lecture8/List61B.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+
 public interface List61B<Item> {
     public void addLast(Item x);
+    public Item get(int i);
     public Item getLast();
     public int size();
     public Item removeLast();
     public void insert(Item x, int position);
     public void addFirst(Item x);
     public void getFirst();
+    default public void print() {
+        for (int i = 0; i < size(); i += 1) {
+            System.out.println(get(i) + " ");
+        }
+        System.out.println();
+    }
 
 }
